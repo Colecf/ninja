1457d2c66d90b54b98c1c89275b8eb21934aba64
Cole Faust <colefaust@google.com>
2023-01-05 17:37:30 -0800

    Compile ninja with C++17
    In preparation for replacing StringPiece with std::string_view,
    which was introduced in C++17.

--- configure.py
+++ configure.py
@@ -351,7 +351,7 @@ else:
               '-Wno-unused-parameter',
               '-fno-rtti',
               '-fno-exceptions',
-              '-std=c++11',
+              '-std=c++17',
               '-fvisibility=hidden', '-pipe',
               '-DNINJA_PYTHON="%s"' % options.with_python]
     if options.debug:
